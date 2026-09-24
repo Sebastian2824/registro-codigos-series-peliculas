c5270c7388c20b9ce9555e370e68623a85d42885
Sebastian2824 <sebasespinoza281204@gmail.com>
2026-09-23 23:47:21 -0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/registro-codigos-series-peliculas/',
   root: './', // Define la carpeta Frontend como raíz
   server: {
     port: 5173,
