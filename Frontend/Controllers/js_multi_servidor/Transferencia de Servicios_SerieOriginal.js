2dbe920c109b01977004393769ecfcdfb7db8a43
Sebastian Espinoza <sebasespinoza281204@gmail.com>
2026-09-26 16:34:02 -0500

    Tu mensaje de commit

--- Frontend/Controllers/js_multi_servidor/Transferencia de Servicios_SerieOriginal.js
+++ Frontend/Controllers/js_multi_servidor/Transferencia de Servicios_SerieOriginal.js
@@ -87,12 +87,13 @@ const logInfo    = document.getElementById('logInfo');
 // UTILIDADES
 // ============================================================
 function escapeHTML(str) {
-    if (!str) return '';
-    return str.replace(/&/g, '&amp;')
-              .replace(/</g, '&lt;')
-              .replace(/>/g, '&gt;')
-              .replace(/"/g, '&quot;')
-              .replace(/'/g, '&#039;');
+    if (str === null || str === undefined) return '';
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
 }
 
 function truncateText(text, maxLength) {
